8ce50f76548c2dc51d89620db02034a6497b0f96
unknown <yakinoki>
2022-08-28 20:45:59 +0900

    20220828

--- svm_training.py
+++ svm_training.py
@@ -1,3 +1,4 @@
 from sklearn.datasets import make_classification
+from sklearn.svm import LinearSVM
 
 x, y = make_classification(n_samples = 100, n_classes = 4, n_features = 10, n_redundant = 3, random_state = 30)
